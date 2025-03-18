c1f8738dc1769b26639e737cfbb77ab474b14205
Fernando V <lvargasp@uni.pe>
2025-03-18 16:22:48 -0500

    Initial Commit

---
+++ SwiftUIMapApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftUIMapApp
+//
+//  Created by FerVP on 18/03/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftUIMapApp/SwiftUIMapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIMapAppApp.swift
+//  SwiftUIMapApp
+//
+//  Created by FerVP on 18/03/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIMapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
